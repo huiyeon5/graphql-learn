0d46230832c417da1ec03c715cdc0be5ef45f728
Huiyeon Kim <gnldus59112@gmail.com>
2018-12-22 19:46:36 +0530

    Added Client

--- server/app.js
+++ server/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+
+mongoose.connect("mongodb://huiyeon5:test123@ds121014.mlab.com:21014/graphql-practice");
+mongoose.connection.once('open', () => {
+    console.log("hello")
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
